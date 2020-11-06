d67469f67edc16836cf0e89df8711923618ac9f8
ninhvanhau <73627123+ninhvanhau@users.noreply.github.com>
2020-11-06 22:59:20 +0700

    Add files via upload

--- SQL – Assignment 4.sql
+++ SQL – Assignment 4.sql
@@ -10,6 +10,10 @@ FROM				`Account`
 WHERE				CreateDate > '2010-12-20';
 
 -- Question 3: Viết lệnh để lấy ra tất cả các developer 
+SELECT *
+FROM `account` 
+JOIN position USING (PositionID)
+WHERE PositionName = 'Dev';
 
 -- Question 4: Viết lệnh để lấy ra danh sách các phòng ban có >3 nhân viên
 SELECT				COUNT(1),DepartmentID,DepartmentName
@@ -19,24 +23,28 @@ GROUP BY 			(DepartmentID)
 HAVING 				COUNT(1) >='3' ;
 
 -- Question 5: Viết lệnh để lấy ra danh sách câu hỏi được sử dụng trong đề thi nhiều nhất
-SELECT 				*,COUNT(1) 
-FROM 				Answer
-LEFT JOIN			Question USING (QuestionID)
+SELECT 				GROUP_CONCAT(ExamID),QuestionID, COUNT(1)
+FROM 				ExamQuestion
 GROUP BY 			QuestionID  
-ORDER BY        	COUNT(1) DESC 
-LIMIT 				1;
+HAVING				COUNT(1)=	(	SELECT 				COUNT(1) 
+									FROM 				ExamQuestion
+									GROUP BY 			QuestionID  
+									ORDER BY        	COUNT(1) DESC 
+									LIMIT 				1)   ;
+
 
 -- Question 6: Thông kê mỗi category Question được sử dụng trong bao nhiêu Question
-SELECT *
+SELECT 				*, COUNT(QuestionID)
 FROM 				CategoryQuestion
-LEFT JOIN 			Question USING (CategoryID) 
-ORDER BY 			CategoryID  ;
+LEFT JOIN 			Question USING (CategoryID)
+GROUP BY  			CategoryID;
+
 
 -- Question 7: Thông kê mỗi Question được sử dụng trong bao nhiêu Exam
-SELECT *
+SELECT 				*,COUNT(QuestionID)
 FROM 				Question
-LEFT JOIN 			Exam USING (CategoryID)
-ORDER BY 			CategoryID  ;
+LEFT JOIN 			ExamQuestion USING (QuestionID)
+GROUP  BY 			QuestionID  ;
 
 -- Question 8: Lấy ra Question có nhiều câu trả lời nhất
 
@@ -46,6 +54,39 @@ LEFT JOIN			Question USING (QuestionID)
 GROUP BY 			QuestionID  
 ORDER BY        	COUNT(1) DESC 
 LIMIT 				1;
+
+-- Question 9: Thống kê số lượng account trong mỗi group 
+SELECT 				*, COUNT(GroupID)
+FROM 				`Group`	
+LEFT JOIN 			`GroupAccount` USING (GroupID)
+GROUP BY  			GroupID;
+
+-- Question 10: Tìm chức vụ có ít người nhất
+SELECT *, COUNT(AccountID) AS SoNguoi
+FROM Position 
+JOIN `account` USING (PositionID)
+GROUP BY PositionID 
+HAVING COUNT(AccountID) = (
+	SELECT COUNT(AccountID)
+	FROM `account`
+	GROUP BY PositionID
+	ORDER BY COUNT(AccountID)
+	LIMIT 1)
+;
+
+
+-- Question 12: Lấy thông tin chi tiết của câu hỏi bao gồm: thông tin cơ bản của  question, loại câu hỏi, ai là người tạo ra câu hỏi, câu trả lời là gì, …
+SELECT *
+FROM Question
+JOIN Answer USING (QuestionID)
+JOIN TypeQuestion USING (TypeID)
+;
+
+-- Question 13: Lấy ra số lượng câu hỏi của mỗi loại tự luận hay trắc nghiệm
+SELECT 				*, COUNT(1)
+FROM 				TypeQuestion	
+JOIN 				Question USING (TypeID)
+GROUP BY  			TypeID;
 
 -- Question 14 && Question15 :Lấy ra group không có account nào
 SELECT *
